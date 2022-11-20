a2d24983ab671f66699884cc7110262346930e3c
osmancoskun <osman.coskun95@gmail.com>
2022-11-20 16:48:13 +0300

    initial commit

---
+++ .routify/routes.js
@@ -0,0 +1,67 @@
+
+/**
+ * @roxi/routify 2.18.8
+ * File generated Wed Nov 16 2022 15:23:00 GMT+0300 (GMT+03:00)
+ */
+
+export const __version = "2.18.8"
+export const __timestamp = "2022-11-16T12:23:00.707Z"
+
+//buildRoutes
+import { buildClientTree } from "@roxi/routify/runtime/buildRoutes"
+
+//imports
+
+
+//options
+export const options = {}
+
+//tree
+export const _tree = {
+  "name": "root",
+  "filepath": "/",
+  "root": true,
+  "ownMeta": {},
+  "absolutePath": "src/pages",
+  "children": [
+    {
+      "isFile": true,
+      "isDir": false,
+      "file": "index.svelte",
+      "filepath": "/index.svelte",
+      "name": "index",
+      "ext": "svelte",
+      "badExt": false,
+      "absolutePath": "/home/osman/Projects/svelte-template/src/pages/index.svelte",
+      "importPath": "../src/pages/index.svelte",
+      "isLayout": false,
+      "isReset": false,
+      "isIndex": true,
+      "isFallback": false,
+      "isPage": true,
+      "ownMeta": {},
+      "meta": {
+        "recursive": true,
+        "preload": false,
+        "prerender": true
+      },
+      "path": "/index",
+      "id": "_index",
+      "component": () => import('../src/pages/index.svelte').then(m => m.default)
+    }
+  ],
+  "isLayout": false,
+  "isReset": false,
+  "isIndex": false,
+  "isFallback": false,
+  "meta": {
+    "recursive": true,
+    "preload": false,
+    "prerender": true
+  },
+  "path": "/"
+}
+
+
+export const {tree, routes} = buildClientTree(_tree)
+
---
+++ src/pages/index.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let count: number = 0;
+</script>
+
+<main
+  class="flex items-center justify-center flex-col gap-3 text-xl h-screen w-screen"
+>
+  <div>
+    <a href="https://svelte.dev/" class="text-orange-600"> Svelte </a>
+    <a href="https://tailwindcss.com/" class="text-blue-600"> TailwindCSS </a>
+    <a href="https://www.typescriptlang.org/" class="text-blue-300">
+      TypeScript
+    </a>
+    <a href="https://www.routify.dev/" class="text-violet-600"> Routify </a>
+    <a href="https://vitejs.dev/" class="text-orange-600"> Vite </a>
+  </div>
+  <button
+    class="button shadow-lg p-3 bg-blue-300 rounded-xl text-white"
+    on:click={() => count++}
+  >
+    Counter {count}
+  </button>
+</main>
