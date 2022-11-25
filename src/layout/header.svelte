7bd34db07d5691bc45bd93e9712e93e3164a22ba
Fidel Choque <devcrash03@gmail.com>
2022-11-25 07:40:46 +0100

    add attibute: alt

--- src/layout/header.svelte
+++ src/layout/header.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="header flex justify-between items-center">
-  <!-- svelte-ignore a11y-missing-attribute -->
-  <a href="/" class="p-2"><img class="h-13 w-20" src={src}/></a>
+  
+  <a href="/" class="p-2"><img class="h-13 w-20" src={src} alt="Fitfinder"/></a>
   <div class="mr-5">
     <a href="/about">About</a>
     <a href="/contact">Contact</a>
