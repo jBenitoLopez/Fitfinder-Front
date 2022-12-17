b3c0666bc5a096995df1a40d79727d09e15e09a0
cursoblv <107802557+jBenitoLopez@users.noreply.github.com>
2022-12-17 13:33:32 +0100

    fix: routes, add empty pages like about and contacts

--- src/layout/header.svelte
+++ src/layout/header.svelte
@@ -5,9 +5,9 @@
 <div class="bg-slate-200 header flex justify-between items-center">
   <a href="/" class="p-2"><img class="h-13 w-20" {src} alt="Fitfinder" /></a>
   <div class="mr-5">
-    <a href="/about" class="bg-blue-400 p-3 rounded text-white">About</a> |
-    <a href="/contact" class="bg-blue-400 p-3 rounded text-white">Contact</a> |
-    <a href="/gyms" class="bg-blue-400 p-3 rounded text-white">Gyms</a> |
+    <a href="/about" class="bg-blue-400 p-3 rounded text-white">About</a>
+    <a href="/contact" class="bg-blue-400 p-3 rounded text-white">Contact</a>
+    <a href="/gyms" class="bg-blue-400 p-3 rounded text-white">Gyms</a>
     <a href="/login" class="bg-blue-400 p-3 ml-4 rounded text-white">Login</a>
   </div>
 </div>
